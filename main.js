4d8a11fc2f6b8ee5040dbb140e35a0d051c63615
Vítor Holtz <vitorholtz27@gmail.com>
2021-01-20 21:10:39 -0300

    tyu

---
+++ main.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+
+    const burger = document.querySelector ('.fa-bars');
+    const close = document.querySelector ('.fa-times');
+    const nav = document.querySelector ('.nav');
+
+    burger.addEventListener('click', () => {
+        nav.style = 'transform: translateX(0%);';
+        close.style = 'display: block';
+        burger.style = 'display: none';
+    });
+
+    close.addEventListener('click', () => {
+        nav.style = 'transform: translateX(100%);'
+        close.style = 'display: none';
+        burger.style = 'display: block';
+    });
+
+    
+
+}
+
+    navSlide ();
